987c36796e7b75ea1458962de2146a45bafea652
k022g0011@g.neec.ac.jp <k022g0011@g.neec.ac.jp>
2023-07-28 14:09:30 +0900

    プロジェクト ファイルを追加します。

---
+++ Behavior.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Ball.h"
+#include "Spring.h"
+#include "Pendulum.h"
+
+void BehaviorSpring(Spring* spring, Ball* ball);
+
+void BehaviorSpring(Spring* spring, Ball* ball, const MyVector3& gravity);
+
+void BehaviorCircleMovement(MyVector3* position, const float& angularVelocity, float* angle, const float& radius, const MyVector3& point);
+
+void BehaviorCircleMovement(Ball* ball, const float& angularVelocity, float* angle, const float& radius, const MyVector3& point);
+
+void BehaviorCircleMovement(MyVector3* position, const float& angularVelocity, float* angle, const MyVector3& point);
+
+void BehaviorCircleMovement(Ball* ball, const float& angularVelocity, float* angle, const MyVector3& point);
+
+void BehaviorPendulum(Pendulum* pendulum);
